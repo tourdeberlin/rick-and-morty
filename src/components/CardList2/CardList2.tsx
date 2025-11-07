8e30a5e0e699ad090f98ae86405ee30c8fc0e4b5
Vera Krumpan <filimonovavera96@gmail.com>
2025-11-07 10:44:26 +0300

    one more try

--- src/components/CardList2/CardList2.tsx
+++ src/components/CardList2/CardList2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './CardList.module.scss';
+import styles from './CardList2.module.scss';
 import CharacterCard from '../CharacterCard/CharacterCard';
 
 import type { CardListProps } from '../types';
--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -6,7 +6,7 @@ import Header from '../components/Header/Header';
 import Loader from '../components/Loader/Loader';
 import NoResults from '../components/NoResults/NoResults';
 import Pagination from '../components/Pagination/Pagination';
-import CardList from '../components/CardList/CardList';
+import CardList2 from '../components/CardList2/CardList2';
 
 const ResultsPage: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>('');
@@ -71,7 +71,7 @@ const ResultsPage: React.FC = () => {
         <Loader />
       ) : searchResults.length ? (
         <>
-          <CardList results={searchResults} />
+          <CardList2 results={searchResults} />
           <div>
             <Pagination
               current={currentPage}
